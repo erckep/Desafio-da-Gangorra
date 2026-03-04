f93dc5925512946493b449676f9a8f206edb7ecd
Erick Pereira <erckeps.dms24@gmail.com>
2026-03-04 18:56:47 -0300

    Primeiro commit do projeto C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+        string linha = Console.ReadLine();
+
+        string[] valores = linha.Split(' ');
+
+        int P1 = int.Parse(valores[0]);
+        int C1 = int.Parse(valores[1]);
+        int P2 = int.Parse(valores[2]);
+        int C2 = int.Parse(valores[3]);
+
+        int TorqueEsquerdo = P1*C1;
+        int TorqueDireito = P2*C2;
+
+        if (TorqueEsquerdo > TorqueDireito)
+        {
+            Console.WriteLine("-1");
+        }
+
+        else if (TorqueEsquerdo < TorqueDireito)
+        {
+            Console.WriteLine("1");
+        }
+
+        else
+        {
+            Console.WriteLine("0");
+        }
+
+
+    }
+}
